d8a9e29bc1a14feca648e4a2004befffc9629687
marispmei <94939570+marispmei@users.noreply.github.com>
2022-02-21 17:17:14 -0300

    Update decodificador.vhd

--- decodificador.vhd
+++ decodificador.vhd
@@ -21,20 +21,20 @@ BEGIN
 	BEGIN
 	
 		CASE entrada_decod IS
-			when "0000" => saida_temp <= "0111111"; -- Display -> 0
-			when "0001" => saida_temp <= "0000110"; -- Display -> 1
-			when "0010" => saida_temp <= "1011011"; -- Display -> 2
-			when "0011" => saida_temp <= "1001111"; -- Display -> 3
-			when "0100" => saida_temp <= "1100110"; -- Display -> 4
-			when "0101" => saida_temp <= "1101101"; -- Display -> 5
-			when "0110" => saida_temp <= "1111101"; -- Display -> 6
-			when "0111" => saida_temp <= "0000111"; -- Display -> 7
-			when "1000" => saida_temp <= "1111111"; -- Display -> 8
-			when "1001" => saida_temp <= "1101111"; -- Display -> 9
-			when "1010" => saida_temp <= "0001000"; -- Display -> traço
-			when "1011" => saida_temp <= "1110011"; -- Display -> P
-			when "1100" => saida_temp <= "0111101"; -- Display -> G
-			when others => saida_temp <= "0001000";
+			when "0000" => saida_temp <= "1000000"; -- Display -> 0
+			when "0001" => saida_temp <= "1111001"; -- Display -> 1
+			when "0010" => saida_temp <= "0100100"; -- Display -> 2
+			when "0011" => saida_temp <= "0110000"; -- Display -> 3
+			when "0100" => saida_temp <= "0011001"; -- Display -> 4
+			when "0101" => saida_temp <= "0010010"; -- Display -> 5
+			when "0110" => saida_temp <= "0000010"; -- Display -> 6
+			when "0111" => saida_temp <= "1111000"; -- Display -> 7
+			when "1000" => saida_temp <= "0000000"; -- Display -> 8
+			when "1001" => saida_temp <= "0010000"; -- Display -> 9
+			when "1010" => saida_temp <= "1110111"; -- Display -> traço
+			when "1011" => saida_temp <= "0001100"; -- Display -> P
+			when "1100" => saida_temp <= "1000010"; -- Display -> G
+			when others => saida_temp <= "1110111";
 		END CASE;
 		
 	END PROCESS;
